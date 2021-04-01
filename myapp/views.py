621f5133fbb55927070ebb3cc7a8d98a667835cb
tjlee93 <dlxowns97@gmail.com>
2021-04-01 19:00:09 +0900

    still fixing

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 # 애플리케이션의 '로직'을 넣는 곳
-# 모델에서 필요한 정보를 받아와서 템플릿에 전달하는 역할 (모델-템플릿 : 연결.)
+# 모델에서 필요한 정보를 받아와서 템플릿에 전달하는 역할 (모델-템플릿 : 연결)
 
 from django.shortcuts import render, get_object_or_404
 from .models import Post
