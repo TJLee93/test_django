f3e583f40d7402b4ca3a23ac6c9ccfa846a5b80c
tjlee93 <dlxowns97@gmail.com>
2021-04-01 15:23:25 +0900

    django 템플릿 생성

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,12 @@
 # 애플리케이션의 '로직'을 넣는 곳
-# 모델에서 필요한 정보를 받아와서 템플릿에 전달하는 역할
+# 모델에서 필요한 정보를 받아와서 템플릿에 전달하는 역할 (모델-템플릿 : 연결)
 
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'myapp/post_list.html', {})
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'myapp/post_list.html', {'posts': posts})
